8cd737ecd80a5d06a50a2b5c01606da8c9a97af5
Roberth <41274329+roberthcobena@users.noreply.github.com>
2021-11-02 11:01:56 -0500

    finalizada
    Realizando pruebas finales

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # Node
  Proyectos con Node y base de datos relacional y no relacional
+
+# Intrucciones
+Utilizar Postman para consumir, esto es un backend rápido, al momento lo he hecho para el entendimiento y uso de datos provenientes de MongoDb, bajo un API REST desarrollado en NODE, peor tambien puede ser usado con base de datos relacinales (SQL - MYSQL)
+
+# Ejemplo: insercion - consulta - modificación - consulta - Eliminación - Consulta
+GET || http://localhost:3000/product
+
+POST || http://localhost:3000/product
+{
+    "name": "pelota",
+    "price": "15.00"
+},
+{
+    "name": "diadema",
+    "price": "10.00"
+}
+
+
+PUT || http://localhost:3000/product/diadema
+{
+    "name": "diadema",
+    "price": "40.00"
+}
+
+DELETE || http://localhost:3000/product/diadema
--- crud_express/server.js
+++ crud_express/server.js
@@ -1,12 +1,67 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const MongoClient = require('mongodb').MongoClient
 
 app.listen(3000, function() {
     console.log('listening on ' + port)
 });
+//CONEXION
+MongoClient.connect('mongodb://localhost/crud-express', { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+    if (err) return console.error(err)
+    console.log('Connected to Database')
+})
 
 //COMPROBAR FUNCIONAMIENTO DE SERVER:
 // app.get('/', (req, res) => {
 //     res.send("Hola Mundo!")
 // })
+
+//VARIABLES para Métodos CRUD
+let db;
+let collection;
+MongoClient.connect('mongodb://localhost/crud-express', { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+    if (err) return console.error(err)
+    console.log('Connected to Database')
+    db = client.db('crud-express')
+    collection = db.collection('product')
+})
+
+//MÉTODOS DEL CRUD: GET (CONSULTA)
+app.get('/product', (req, res) => {
+    db.collection('product').find().toArray()
+        .then(results => {
+            res.json(results);
+        }).catch(error => console.error(error));
+})
+
+//MÉTODOS DEL CRUD: POST
+app.post('/product', (req, res) => {
+    collection.insertOne(req.body)
+        .then(result => {
+            res.json('Success');
+        })
+        .catch(error => console.error(error))
+})
+
+//MÉTODOS DEL CRUD: PUT
+app.put('/product/:id', (req, res) => {
+    collection.findOneAndUpdate({ name: req.params.id }, {
+            $set: {
+                name: req.body.name,
+                price: req.body.price
+            }
+        }, {
+            upsert: true
+        }).then(result => { res.json('Updated') })
+        .catch(error => console.error(error))
+});
+
+//MÉTODOS DEL CRUD: DELETE
+app.delete('/product/:id', (req, res) => {
+    collection.deleteOne({ name: req.params.id })
+        .then(result => {
+            res.json('Deleted')
+        })
+        .catch(error => console.error(error))
+})
